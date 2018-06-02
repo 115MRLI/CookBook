e5c7f979abfb8328739b9e043d7dac470ba174c7
115MRLI <1159642490@qq.com>
2018-06-02 17:11:42 +0800

    改变菜单样式

--- app/src/main/java/book/cook/view/activity/MainActivity.java
+++ app/src/main/java/book/cook/view/activity/MainActivity.java
@@ -1,39 +1,28 @@
 package book.cook.view.activity;
-
-import android.animation.ObjectAnimator;
-import android.animation.ValueAnimator;
-import android.annotation.SuppressLint;
-import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.view.View;
-import android.view.animation.AccelerateInterpolator;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.Toast;
+
+import com.dxtt.coolmenu.CoolMenuFrameLayout;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import book.cook.R;
-import book.cook.app.App;
 import book.cook.base.BaseActivity;
-import book.cook.view.widget.FoldingLayout;
-import book.cook.view.widget.OnFoldListener;
+import book.cook.view.fragment.Fragment1;
+import book.cook.view.fragment.Fragment2;
+import book.cook.view.fragment.Fragment3;
+import book.cook.view.fragment.Fragment4;
 import butterknife.BindView;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
-    @BindView(R.id.traffic_layout)
-    LinearLayout mTrafficLayout;
-    @BindView(R.id.life_layout)
-    LinearLayout mLifeLayout;
-    @BindView(R.id.traffic_bar_layout)
-    RelativeLayout mTrafficBarLayout;
-    @BindView(R.id.life_bar_layout)
-    RelativeLayout mLifeBarLayout;
-    @BindView(R.id.traffic_item)
-    FoldingLayout mTrafficFoldingLayout;
-    @BindView(R.id.life_item)
-    FoldingLayout mLifeFoldingLayout;
-    @BindView(R.id.bottom_view)
-    View mBottomView;
-    private final int FOLD_ANIMATION_DURATION = 500;
-    private int mNumberOfFolds = 2;
+    @BindView(R.id.rl_main)
+    CoolMenuFrameLayout coolMenuFrameLayout;
+
+    List<String> titleList = null;
+    List<Fragment> fragments = new ArrayList<>();
     @Override
     protected int getLayout() {
         return R.layout.activity_main;
@@ -42,127 +31,45 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     protected void initView() {
         super.initView();
-        //设置子菜单折叠层数
-        mTrafficFoldingLayout.setNumberOfFolds(mNumberOfFolds);
-        mLifeFoldingLayout.setNumberOfFolds(mNumberOfFolds);
-        //将打开的子菜单进行关闭
-        animateFold(mTrafficFoldingLayout, FOLD_ANIMATION_DURATION);
-        animateFold(mLifeFoldingLayout, FOLD_ANIMATION_DURATION);
-        setMarginToTop(1, mLifeLayout);
-        setMarginToTop(1, mBottomView);
-
+        String[] titles = {"美食精选", "电饭煲食谱", "早餐食谱", "懒人菜谱"};
+        titleList = Arrays.asList(titles);
+        //set your titles,which is optional
+        coolMenuFrameLayout.setTitles(titleList);
+        //set your menu icon
+        coolMenuFrameLayout.setMenuIcon(R.mipmap.menu);
     }
 
     @Override
     protected void initEvent() {
         super.initEvent();
-        mTrafficBarLayout.setOnClickListener(this);
-        mLifeBarLayout.setOnClickListener(this);
 
     }
 
     @Override
     protected void getIntentData() {
         super.getIntentData();
-    }
-
-    private void handleAnimation(final View bar, final FoldingLayout foldingLayout, final View nextParent) {
-
-
-        foldingLayout.setFoldListener(new OnFoldListener() {
-
+        fragments.add(new Fragment1());
+        fragments.add(new Fragment2());
+        fragments.add(new Fragment3());
+        fragments.add(new Fragment4());
+        FragmentPagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
-            public void onStartFold(float foldFactor) {
-
-//                bar.setClickable(true);
-                resetMarginToTop(foldingLayout, foldFactor, nextParent);
+            public Fragment getItem(int position) {
+                return fragments.get(position);
             }
 
             @Override
-            public void onFoldingState(float foldFactor, float foldDrawHeight) {
-//                bar.setClickable(false);
-                resetMarginToTop(foldingLayout, foldFactor, nextParent);
+            public int getCount() {
+                return fragments.size();
             }
-
-            @Override
-            public void onEndFold(float foldFactor) {
-//                bar.setClickable(true);
-                resetMarginToTop(foldingLayout, foldFactor, nextParent);
-            }
-        });
-
-//      foldingLayout.setNumberOfFolds(mNumberOfFolds);
-        animateFold(foldingLayout, FOLD_ANIMATION_DURATION);
-
+        };
+        coolMenuFrameLayout.setAdapter(adapter);
     }
-
-    /**
-     * 展开动画
-     *
-     * @param foldLayout
-     * @param duration
-     */
-    @SuppressLint("NewApi")
-    public void animateFold(FoldingLayout foldLayout, int duration) {
-        float foldFactor = foldLayout.getFoldFactor();
-        ObjectAnimator animator= ObjectAnimator.ofFloat(foldLayout, "foldFactor", foldFactor, foldFactor > 0 ? 0 : 1);
-        animator.setRepeatMode(ValueAnimator.REVERSE);
-        animator.setRepeatCount(0);
-        animator.setDuration(duration);
-        animator.setInterpolator(new AccelerateInterpolator());
-        animator.start();
-    }
-
-    private void setMarginToTop(float foldFactor, View nextParent) {
-
-        LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) nextParent.getLayoutParams();
-        lp.topMargin = (int) (-dp2px(MainActivity.this, 100) * foldFactor) + dp2px(MainActivity.this, 10);
-        nextParent.setLayoutParams(lp);
-    }
-
-    public final static int dp2px(Context context, float dpValue) {
-        float density = context.getResources().getDisplayMetrics().density;
-        return (int) (dpValue * density + 0.5f);
-    }
-
-    private void resetMarginToTop(View view, float foldFactor, View nextParent) {
-
-        LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) nextParent.getLayoutParams();
-        lp.topMargin = (int) (-view.getMeasuredHeight() * foldFactor) + dp2px(MainActivity.this, 10);
-        nextParent.setLayoutParams(lp);
-    }
-
-//    @Override
-//    protected void onRestart() {
-//        super.onRestart();
-////        //将打开的子菜单进行关闭
-////        animateFold(mTrafficFoldingLayout, FOLD_ANIMATION_DURATION);
-////        animateFold(mLifeFoldingLayout, FOLD_ANIMATION_DURATION);
-////        setMarginToTop(1, mLifeLayout);
-////        setMarginToTop(1, mBottomView);
-//
-//        mLifeBarLayout.setClickable(true);
-//        mTrafficBarLayout.setClickable(true);
-//        Toast.makeText(App.mContext,"mLifeBarLayout  :"+ mLifeBarLayout.isClickable(),Toast.LENGTH_SHORT).show();
-//        Toast.makeText(App.mContext,"mTrafficBarLayout  :"+ mTrafficBarLayout.isClickable(),Toast.LENGTH_SHORT).show();
-////        initEvent();
-//        mTrafficBarLayout.setOnClickListener(this);
-//        mLifeBarLayout.setOnClickListener(this);
-//    }
 
 
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.life_bar_layout:
-                Toast.makeText(App.mContext,"点击了",Toast.LENGTH_SHORT).show();
-                handleAnimation(v, mLifeFoldingLayout, mBottomView);
-                break;
-            case R.id.traffic_bar_layout:
-                Toast.makeText(App.mContext,"点击了",Toast.LENGTH_SHORT).show();
-                handleAnimation(v, mTrafficFoldingLayout, mLifeLayout);
-                break;
-        }
+
     }
 }
